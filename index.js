591afbc3950a13da8f7cf46fa0f854986958057a
Doreen <doreenlulu97@gmail.com>
2022-08-06 22:54:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
     employee2[key] = value;
     return employee2;
 }
-function destructivelyUpdatedEmployeeWithKeyValue(employee, key, value) {
-    return {
-        ...employee,
-        [key]: value,
-    };
-}
-function deleteFromEmployeeByKey (employee, key) {
-    delete employee[key];
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
     return employee;
 }
-function destructivelyDeleteFromEmployeeByKey(employee, employee2, key) {
-    delete employee2[key];
+
+function deleteFromEmployeeByKey (employee, key) {
+    const employeeNew = {...employee};
+    delete employeeNew[key];
+    return employeeNew;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
     return employee;
 }
